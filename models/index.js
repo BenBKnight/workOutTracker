85ca68ca59195b912cbdbbd1a794c6e71d68c9d2
Ben K <ben.knight.brazil@hotmail.com>
2020-06-24 18:19:28 -0600

    Added html routes

---
+++ models/index.js
@@ -0,0 +1,1 @@
+module.exports = mongoose.model('workout.js', workout)
---
+++ routes/api-routes.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const db = require("../models");
+const { Mongoose } = require("mongoose");
+
+module.exports = mongoose.model('workout', workout)
